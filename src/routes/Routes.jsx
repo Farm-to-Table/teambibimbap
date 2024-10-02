0feb74542d12a7a0324ee68fcf559a35d51fd85a
henryHongStudent <115530962+HenryHong91@users.noreply.github.com>
2024-10-02 15:47:54 +1300

    change to case sensetive

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,13 @@
-import { createBrowserRouter, Outlet } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Cart from "../Pages/cart/Cart";
 import App from "../App";
-
 import Home from "../Pages/Home/Home";
 import FarmDetailPage from "../Pages/Farm_Detail_page/FarmDetailPage";
 import Map from "../Pages/Map/Map";
 import Vege from "../Pages/Vege/Vege";
 import Salad from "../Pages/Salad/Salad";
 import Fruit from "../Pages/Fruit/Fruit";
-import ProductPage from "../Pages/productPage/productPage";
+import ProductPage from "../Pages/ProductPage/ProductPage";
 import Payment from "../Pages/Payment/Payment";
 
 export const router = createBrowserRouter([
